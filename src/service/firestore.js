01673405e4cb2f6e7abee7ec5bb1ad714bbcc82c
Erick Milan <erickmilan1002@gmail.com>
2022-05-31 10:24:57 -0500

    agregando credenciales

---
+++ src/service/firestore.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIX6GwNTrCONjvdIsaYZewo8_0AoVdMVg",
+  authDomain: "foods-restaurant-cc832.firebaseapp.com",
+  projectId: "foods-restaurant-cc832",
+  storageBucket: "foods-restaurant-cc832.appspot.com",
+  messagingSenderId: "714597763036",
+  appId: "1:714597763036:web:39f8efdef73a0d5baa3119"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
